8d3cc5548d85505bdcd9e1ce12e7edc087ca4405
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2021-07-19 00:36:37 -0700

    removing spurious quantities

--- pipeline/exp/tweet_vaccine_reports/main.py
+++ pipeline/exp/tweet_vaccine_reports/main.py
@@ -145,10 +145,6 @@ def tweet_vax_ranking(_):
     twitter.update_status(
             status="{} Top 5 states-% of both doses vaxxed 18+ pop\n{}".format(str(today.date()), pretty_print_dataframe(overallPercentage[:5]))
             )
-    twitter.update_status(
-            status="{} Bottom 5 states-% of both doses vaxxed 18+ pop\n{}".format(str(today.date()), pretty_print_dataframe(overallPercentage[-5:]))
-            )
-
 
     return "OK!"
 
